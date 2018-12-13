d7e996646dc56443c5cc6453f54a40aed39ece3b
garryoldman <45848006+garryoldman@users.noreply.github.com>
2018-12-14 02:34:38 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -64,8 +64,14 @@ musql $renewrepo < $renerepo/dump.sql
 }
 
 function installreq {
-    chmod +x $lampdir/install_requiments.sh
-    sh $lampdir/install_requiments.sh
+yum update && yum install -y git yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo  https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --enable docker-ce-edge
+yum install -y docker-ce
+systemctl start docker
+systemctl enable docker
+curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
 }
 
 function dockerbuild {
